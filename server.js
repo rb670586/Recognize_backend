9b6bacce0b7236ada03543f38931637060474e21
ryanbb <ryanbb@comcast.net>
2019-06-09 18:48:27 -0400

    changing response

--- server.js
+++ server.js
@@ -24,8 +24,7 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> {res.send(database.users);
-})
+app.get('/', (req, res)=> { res.send('it is working!') })
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)}) //dependency injection
