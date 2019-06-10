3f08b85510616191c4524e3214e8277367eefd63
ryanbb <ryanbb@comcast.net>
2019-06-09 20:10:14 -0400

    changes to controllers/register.js

--- controllers/register.js
+++ controllers/register.js
@@ -1,9 +1,10 @@
 
 const handleRegister = (req, res, db, bcrypt) => {
+  const { email, name, password } = req.body;
   if (!email || !name || !password) {
     return res.status(400).json('incorrect form submission')
   }
-  const { email, name, password } = req.body;
+  
   const hash = bcrypt.hashSync(password);
     db.transaction(trx => {
       trx.insert({
